f74a7b2ca9bf66d4357575799b1ee389c05b30a0
NareshSanjeev <naresh732003@gmail.com>
2026-01-09 18:46:47 +0530

    Add CORS support and update package dependencies

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 const express = require("express");
 require('dotenv').config();
-
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/auth", require("./routes/authRoutes"));
